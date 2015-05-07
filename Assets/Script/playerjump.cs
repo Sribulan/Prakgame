9f6f022cf2c33aefaf5c7a2dbc29da988f864bf1
sribulan <60200112049@uin-alauddin.ac.id>
2015-05-07 12:59:39 +0800

    test cuy

---
+++ Assets/Script/playerjump.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerjump : MonoBehaviour {
+	float Lompat;
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+		audio.Play();
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.up * 100);		
+		}
+	}
+}
---
+++ Assets/Script/playermove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playermove : MonoBehaviour {
+
+	Vector3 position;
+	bool jump;
+	float speedmove=3;
+	float speedjump=200;
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+            audio.Play();
+			position= transform.position+Vector3.left;
+			this.gameObject.transform.position = Vector3.MoveTowards (transform.position, position, speedmove * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+            audio.Play();
+			position= transform.position+Vector3.right;
+			this.gameObject.transform.position = Vector3.MoveTowards (transform.position, position, speedmove * Time.deltaTime);
+		}
+		if (!jump) {
+			if (Input.GetKey (KeyCode.Space)) {
+                audio.Play();
+				GetComponent<Rigidbody> ().velocity = Vector3.zero;
+				GetComponent<Rigidbody> ().AddForce (Vector3.up * speedjump);		
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		//Debug.Log ("Tersentuh");
+        if (other.gameObject.tag == "Point")
+            other.gameObject.audio.Play();
+	}
+
+	void OnCollisionExit(Collision other){
+		//Debug.Log ("Terlepas");
+        if (other.gameObject.tag == "Point")
+            other.gameObject.audio.Stop();
+	}
+}
